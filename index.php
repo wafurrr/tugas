27b864c6d866521f092292de966bcb0cd0d6ae3e
wapcuy <wapcuyahaydek@gmail.com>
2020-09-13 17:29:53 +0700

    update index.php class anjing

--- index.php
+++ index.php
@@ -22,3 +22,23 @@ echo " Bisa terbang: $Kucing->bisa_terbang";
 echo "<br>";
 echo " Suara: $Kucing->suara";
 echo "<hr>"; 
+
+class Anjing{
+	public $nama, $jumlah_kaki, $bisa_terbang, $suara;
+}
+
+$Anjing = new Anjing;
+$Anjing->nama = "GokGok";
+$Anjing->jumlah_kaki = "4";
+$Anjing->bisa_terbang = "tidak";
+$Anjing->suara = "Guk Guk Guk";
+
+echo "Anjing <br>";
+echo " Nama: $Anjing->nama";
+echo "<br>";
+echo " Jumlah kaki: $Anjing->jumlah_kaki";
+echo "<br>";
+echo " Bisa terbang: $Anjing->bisa_terbang";
+echo "<br>";
+echo " Suara: $Anjing->suara";
+echo "<hr>";
